bec2ddff9cd564dfc7fd54c5de6293e39e8dc29d
Mateus Santos <msantos@wikimedia.org>
2018-12-19 12:50:35 -0200

    Adding .gitignore, README and perform some minor cleanup.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tilerator analyzer
+
+Tilerator analyzer is a Nodejs tool that collect data from the tilerator API to manipulate it for metrics analysis.
+
+It was built to manually monitor full planet tile generation, which is a long and delicate process.
+
+## Usage
+
+For a better use of this tool make sure that:
+- The tilerator Queue is empty before generating full planet tile generation. 
+- Use the flag `-j.keepJob=true` when generating tiles
+
+Log into the instance you want to analyze, for example:
+```
+$ ssh -L 6535:localhost:6535 maps1004.eqiad.wmnet
+```
+
+Start the tool with `npm start`.
+
+**The tool will generate logs for each kind of state tilerator have for its jobs and one main log with overall status.**
+
+## TODO
+[] Improve metric collection
+[] Move storage from log files to a proper DB
--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const   fs = require('fs'),
         log = require('./lib/log'),
         conf = yaml.safeLoad(fs.readFileSync('./config.dev.yaml', 'utf8'))
 
-dashboard.fetchByState('active').then((completedJobs) => {
-    // console.log(completedJobs);
-    log(completedJobs, `${conf.use}/active`);
-    completedJobs.forEach((element) => {
-        delete element.jobs;
+const states = [
+    'complete',
+    'active',
+    'inactive',
+    'delayed',
+    'failed',
+]
+
+states.map((state) => {
+    dashboard.fetchByState(state).then((completedJobs) => {
+        log(stateJobs, `${conf.use}/${state}`);
+        stateJobs.forEach((element) => {
+            delete element.jobs;
+        });
+        delete stateJobs.jobs;
+        stateJobs.map((collection) => {
+            console.log(collection.missing);
+        }); 
     });
-    delete completedJobs.jobs;
-    console.log(completedJobs);
-    // completedJobs.map((collection) => {
-    //     console.log(collection.missing);
-    // }); 
 });
+
 
 dashboard.fetchAll().then((allJobs) => {
     let all = dashboard.getOverallStats();
     log(all, `${conf.use}/main`);
-    console.log(all);
-    // all.map((collection) => {
-    //     console.log(collection);
-    // }); 
+    // console.log(all);
 });
